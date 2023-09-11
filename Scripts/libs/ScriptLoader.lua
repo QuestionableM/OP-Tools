a75805c65ebae762df07b6fe7c9fb9608fd0bbe0
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2023-09-10 22:56:07 -0700

    Spreading awareness

--- Scripts/libs/ScriptLoader.lua
+++ Scripts/libs/ScriptLoader.lua
@@ -5,6 +5,25 @@
 if OP_TOOLS_SCRIPT_LOADED then return end
 OP_TOOLS_SCRIPT_LOADED = true
 
+--Self check the description for infections
+if type(op_jsonOpen) == "function" and type(op_chatMessage) == "function" then
+	local descr_data = op_jsonOpen("$CONTENT_DATA/description.json")
+	if descr_data then
+		local dependencies = descr_data.dependencies
+		if type(dependencies) == "table" and #dependencies > 0 then
+			op_chatMessage("#ff0000WARNING#ffffff: OP Tools is infected. Here's the list of infections:")
+
+			for k, dependency in pairs(dependencies) do
+				local dep_id = dependency.fileId or 0
+				local dep_name = dependency.name or "UNKNOWN"
+				op_chatMessage(("%i -> SteamId: #ffff00%i#ffffff Name: #ffff00%s#ffffff"):format(k, dep_id, dep_name))
+			end
+
+			op_chatMessage("These mods open backdoors in your game or waiting for a certain amount of people to get infected to start the payload. Please report them as soon as possible.")
+		end
+	end
+end
+
 dofile("Functions.lua")
 dofile("GuiStuff.lua")
 
---
+++ Scripts/libs/SelfCheck.lua
@@ -0,0 +1,4 @@
+op_jsonOpen = sm.json.open
+op_chatMessage = sm.gui.chatMessage
+
+SelfCheck = class()
